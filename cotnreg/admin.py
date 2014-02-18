dc800a65e8542e80c640e81918d87cb2e7d6dff0
Albert Choy <albert.choy@osii.com>
2014-02-17 23:15:21 -0600

    stuff

--- cotnreg/admin.py
+++ cotnreg/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from cotnreg.models import User, UserReg
+from cotnreg.models import *
 
 admin.site.register(User)
 admin.site.register(UserReg)
+admin.site.register(Membership)
+admin.site.register(Catalog)
